95d958197e5044b54bb7e2fe9cd7672569fb42be
Errafai <errafaiachraf1923@gmail.com>
2024-12-21 11:30:24 +0100

    linking database with the website

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Post = require('../models/Post');
+
+/**
+ * GET /
+ * HOME
+ */
+router.get('',  async (req, res) => {
+    
+
+    try {
+        let perPage = 10;
+        let page  = req.query.page || 1;
+        const data = await Post.aggregate([ {$sort: {createdAt: -1} } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
 
-router.get('', (req, res) => {
-    res.render("index");
+        res.render("index", {data,
+            current: page,
+        nextPage: hasNextPage ? nextPage: null
+     });
+        
+    } catch (error) {
+       console.log(error); 
+    }
 });
+
 router.get('/about', (req, res) => {
     res.render("about");
 });
